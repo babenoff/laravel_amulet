2364d6b1f18866f927f6d313b4731872ace48d93
Maks Babenko <mb.babenoff@yandex.ru>
2016-09-25 16:38:11 +0300

    favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,6 +12,7 @@
 
     <!-- Styles -->
     <link href="/css/app.css" rel="stylesheet">
+    <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
 
     <!-- Scripts -->
     <script>
